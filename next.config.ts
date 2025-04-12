1c522ea8eb62d92bf41a59904974a9ca88947c85
trymeuj <ujjwalmathuriit@gmail.com>
2025-04-13 01:14:13 +0530

    github deploy setup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/aiva-landing-page',
+  assetPrefix: '/aiva-landing-page',
 };
 
 export default nextConfig;
